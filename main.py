366349de2d4a9ad6df785b0fd3461499dc82efcf
AlexBazovkin <alexbaz@vk.com>
2022-06-27 19:18:16 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+nested_list = [
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f', 'h', False],
+    [1, 2, None],
+]
+
+
+class MyClass:
+    def __init__(self, incoming_list, x: int = 0, y: int = 0):
+        self.incoming_list = incoming_list
+        self.x = x
+        self.y = y
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.x != len(self.incoming_list):
+            if self.y < len(self.incoming_list[self.x]):
+                result = self.incoming_list[self.x][self.y]
+                self.y += 1
+                if self.y == len(self.incoming_list[self.x]):
+                    self.y -= len(self.incoming_list[self.x])
+                    self.x += 1
+        else:
+            raise StopIteration
+        return result
+
+
+# Вывод каждого элемента -----------------
+for item in MyClass(nested_list):
+    print(item)
+
+print("*"*20)
+
+# Вывод в одну строку -------------------
+flat_list = [item for item in MyClass(nested_list)]
+print(flat_list)
+
+print("*"*30)
+
+
+# Создание и вывод генератора ----------------
+def flat_generator(incoming_list):
+    for list_ in incoming_list:
+        for el in list_:
+            yield el
+
+
+for item in flat_generator(nested_list):
+    print(item)
